8012ff21a0c6609d27553d6d669e6577b8fd79a0
yukimine <xingyanjintian@gmail.com>
2026-05-22 15:56:12 +0900

    tauriの動作実験を実施

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,21 @@ import { invoke } from "@tauri-apps/api/core";
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
 
-async function greet() {
+async function test_tauri() {
   if (greetMsgEl && greetInputEl) {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
+    try{
+      greetMsgEl.textContent = await invoke("test_tauri", {
+      num: Number(greetInputEl.value,)
     });
+  }catch (e){console.log(e)}
   }
 }
-
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
     e.preventDefault();
-    greet();
+    test_tauri()
   });
 });
